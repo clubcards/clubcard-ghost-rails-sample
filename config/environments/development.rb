a21ece752f82c2d049592e17846caab83fcf1a2a
Michiel Sikkes <michiel@firmhouse.com>
2021-12-31 09:36:15 +0100

    Update local hostname

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -71,5 +71,5 @@ Rails.application.configure do
   # Raise error when a before_action's only/except options reference missing actions
   config.action_controller.raise_on_missing_callback_actions = true
 
-  config.hosts << "clubcard-demo-ghost.test"
+  config.hosts << "clubcard-ghost-rails-sample.test"
 end
